3e7e54ebacddeda08f1cf031fbc1115315923eb1
Matthieu Falce <matthieu@falce.net>
2025-10-01 17:01:22 +0200

    gitautosync update 29

--- tape_en_cours/list_dirs.py
+++ tape_en_cours/list_dirs.py
@@ -2,3 +2,9 @@ from pathlib import Path
 
 p = Path(".")
 print([x for x in p.iterdir() if x.is_dir()])
+
+# on peut lister les fichiers récursivement et prendre uniquement les fichiers qui
+# ont un nom d'une longueur supérieure à 4 caractères
+#
+#
+# et les zipper dans compressed.zip
