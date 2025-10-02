edbb2aa855bc9aed2da02872b6ba05a254f80756
Matthieu Falce <matthieu@falce.net>
2025-10-02 14:40:48 +0200

    gitautosync update 142

--- tape_en_cours/archive.py
+++ tape_en_cours/archive.py
@@ -14,13 +14,25 @@ app = typer.Typer()
 
 
 @app.command()
-def create(
+def archive(
     path: str,
     max_length: int = 2,
     zip_name: str = "multiple_files.zip",
+) -> None:
+    """Crée une archive zip des fichiers Python avec des noms courts."""
+    p = Path(path)
+    all_files = get_all_files(p)
+    python_files = filter_python_files(all_files)
+    short_named_files = filter_short_named_files(python_files, max_length)
+    generate_zip_archive(short_named_files, zip_name)
+
+@app.command()
+def report(
+    path: str,
+    max_length: int = 2,
     output_txt: str = "short_named_python_files.txt",
 ) -> None:
-    """Fonction principale pour exécuter le script."""
+    """Génère un rapport texte des fichiers Python avec des noms courts."""
     p = Path(path)
     all_files = get_all_files(p)
     python_files = filter_python_files(all_files)
