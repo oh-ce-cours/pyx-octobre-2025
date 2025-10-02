8af08eaf92bdc36bfc2fa363c725a82986d488fc
Matthieu Falce <matthieu@falce.net>
2025-10-02 09:47:45 +0200

    gitautosync update 14

--- tape_en_cours/list_dirs.py
+++ tape_en_cours/list_dirs.py
@@ -6,10 +6,9 @@
 #
 # et les zipper dans compressed.zip
 
-from pathlib import Path
 import zipfile
 
-p = Path(".")
+p = zipfile.Path(".")
 files_to_zip = []
 for x in p.rglob("*.py"):
     if x.is_file() and len(x.stem) < 2:
