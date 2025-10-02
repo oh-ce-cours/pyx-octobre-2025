fc1c950e2106881a5b87e52d045a5c05f90c4e25
Matthieu Falce <matthieu@falce.net>
2025-10-02 12:23:02 +0200

    gitautosync update 108

--- tape_en_cours/archive.py
+++ tape_en_cours/archive.py
@@ -8,11 +8,11 @@ from list_dirs_improved import (
 )
 
 
-# def main() -> None:
-#     """Fonction principale pour exécuter le script."""
-#     p = Path(".")
-#     all_files = get_all_files(p)
-#     python_files = filter_python_files(all_files)
-#     short_named_files = filter_short_named_files(python_files, 2)
-#     # output_file_list(short_named_files, "short_named_python_files.txt")
-#     generate_zip_archive(short_named_files, "multiple_files.zip")
+def main() -> None:
+    """Fonction principale pour exécuter le script."""
+    p = Path(".")
+    all_files = get_all_files(p)
+    python_files = filter_python_files(all_files)
+    short_named_files = filter_short_named_files(python_files, 2)
+    # output_file_list(short_named_files, "short_named_python_files.txt")
+    generate_zip_archive(short_named_files, "multiple_files.zip")
