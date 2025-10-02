137f58549b60740ba1668faae28e65d1c07def31
Matthieu Falce <matthieu@falce.net>
2025-10-02 11:39:05 +0200

    gitautosync update 62

--- tape_en_cours/list_dirs_improved.py
+++ tape_en_cours/list_dirs_improved.py
@@ -5,6 +5,10 @@ def get_all_files(path):
     return [x for x in path.rglob("*") if x.is_file()]
 
 
+def filter_python_files(files):
+    return [f for f in files if f.suffix == ".py"]
+
+
 p = Path(".")
 all_files = get_all_files(p)
 print(all_files)
