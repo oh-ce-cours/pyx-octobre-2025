70ef6ebcf599f33a6508a942280ca04a8744fa49
Matthieu Falce <matthieu@falce.net>
2025-10-02 12:16:09 +0200

    gitautosync update 89

--- tape_en_cours/list_dirs_improved.py
+++ tape_en_cours/list_dirs_improved.py
@@ -73,9 +73,15 @@ def generate_zip_archive(files: list[Path], zip_name: str) -> None:
             archive.write(filename, arcname=filename.name)
 
 
-p = Path(".")
-all_files = get_all_files(p)
-python_files = filter_python_files(all_files)
-short_named_files = filter_short_named_files(python_files, 2)
-# output_file_list(short_named_files, "short_named_python_files.txt")
-generate_zip_archive(short_named_files, "multiple_files.zip")
+def main() -> None:
+    """Fonction principale pour exécuter le script."""
+    p = Path(".")
+    all_files = get_all_files(p)
+    python_files = filter_python_files(all_files)
+    short_named_files = filter_short_named_files(python_files, 2)
+    # output_file_list(short_named_files, "short_named_python_files.txt")
+    generate_zip_archive(short_named_files, "multiple_files.zip")
+
+
+if __name__ == "__main__":
+    main()
