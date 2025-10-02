29791add6605636bbc5fb34fcd5fb6c209407e2e
Matthieu Falce <matthieu@falce.net>
2025-10-02 14:43:41 +0200

    gitautosync update 149

--- tape_en_cours/list_dirs_improved.py
+++ tape_en_cours/list_dirs_improved.py
@@ -70,7 +70,7 @@ def generate_zip_archive(files: list[Path], zip_name: str) -> None:
     """
     with zipfile.ZipFile(zip_name, mode="w") as archive:
         for filename in files:
-            file_exists = archive
+            file_exists = filename.name in archive.namelist()
             print(filename.name, file_exists)
             archive.write(filename, arcname=filename.name)
 
