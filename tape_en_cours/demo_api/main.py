ee4f82ee8c2417f5136d2fc90969adb64878a826
Matthieu Falce <matthieu@falce.net>
2025-10-03 15:07:26 +0200

    gitautosync update 353

--- tape_en_cours/demo_api/main.py
+++ tape_en_cours/demo_api/main.py
@@ -10,7 +10,7 @@
 # ...
 
 
-from utils.api import get_users, get_vms, add_vms_to_users
+from utils.api import get_users, get_vms, add_vms_to_users, create_user
 import json
 
 BASE_URL = "https://x8ki-letl-twmt.n7.xano.io/api:N1uLlTBt"
