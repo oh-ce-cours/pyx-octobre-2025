a005030742e5839061f51464974988844bdf3a59
Matthieu Falce <matthieu@falce.net>
2025-10-03 14:19:54 +0200

    gitautosync update 306

---
+++ tape_en_cours/# on veut récupérer la liste des utilisa.py
@@ -0,0 +1,13 @@
+# on veut récupérer la liste des utilisateurs
+# on veut récupérer la liste des VMs
+# on veut que pour un utilisateur je puisse avoir la liste de ses VMs
+
+# on veut faire un rapport qui regroupe les VM par état (status)
+# VM running: 10
+# VM stopped: 5
+# VM paused: 2
+# ...
+
+import requests
+import datetime
+import json
