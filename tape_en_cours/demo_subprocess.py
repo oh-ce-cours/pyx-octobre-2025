41e60c7200b44c40c49dda50d1397d832f69edec
Matthieu Falce <matthieu@falce.net>
2025-10-03 10:12:22 +0200

    gitautosync update 255

--- tape_en_cours/demo_subprocess.py
+++ tape_en_cours/demo_subprocess.py
@@ -1,1 +1,12 @@
 import subprocess
+
+try:
+    result = subprocess.run(['ls'], capture_output=True, text=True, check=True)
+    output = result.stdout
+    print("Résultat de ls :")
+    print(output)
+except subprocess.CalledProcessError as e:
+    print(f"Erreur lors de l'exécution de ls: {e}")
+    print(f"Sortie d'erreur : {e.stderr}")
+except Exception as e:
+    print(f"Une erreur inattendue est survenue : {e}")
