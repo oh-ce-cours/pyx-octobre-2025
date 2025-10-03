157cfb302d1329a1d4e819738924bac291e32f86
Matthieu Falce <matthieu@falce.net>
2025-10-03 10:46:47 +0200

    gitautosync update 283

--- tape_en_cours/demo_subprocess.py
+++ tape_en_cours/demo_subprocess.py
@@ -11,7 +11,7 @@ file_handler = logging.FileHandler("plop.log")
 file_handler.setFormatter(
     logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 )
-file_handler.setLevel(logging.ERROR)
+file_handler.setLevel(logging.DEBUG)
 logger.addHandler(file_handler)
 
 # Stream handler pour tous les niveaux
