60f88049c7589149b6337ba05694711c8683a065
Matthieu Falce <matthieu@falce.net>
2025-10-04 17:50:14 +0200

    gitautosync update 932

--- tape_en_cours/demo_api/utils/__init__.py
+++ tape_en_cours/demo_api/utils/__init__.py
@@ -1,1 +1,7 @@
-# Module utils pour demo_api
+"""
+Module d'utilitaires pour demo_api.
+
+Ce module contient tous les utilitaires nécessaires au fonctionnement
+de l'application, incluant la configuration, le logging, la génération
+de données et les services métier.
+"""
