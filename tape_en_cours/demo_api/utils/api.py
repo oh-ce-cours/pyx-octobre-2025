56bf254baad162aa06da3d51a7bb3f2222085b08
Matthieu Falce <matthieu@falce.net>
2025-10-04 13:28:55 +0200

    gitautosync update 434

--- tape_en_cours/demo_api/utils/api.py
+++ tape_en_cours/demo_api/utils/api.py
@@ -7,6 +7,7 @@ from .api_auth import Auth
 # Initialisation de l'instance Auth globale
 _auth_instance = None
 
+
 def _get_auth_instance(base_url):
     """Retourne l'instance Auth singleton"""
     global _auth_instance
@@ -14,16 +15,19 @@ def _get_auth_instance(base_url):
         _auth_instance = Auth(base_url)
     return _auth_instance
 
+
 # Wrapper functions pour simplifier l'utilisation
 def create_user(base_url, name, email, password):
     """Crée un nouvel utilisateur"""
     auth = _get_auth_instance(base_url)
     return auth.create_user(name, email, password)
 
+
 def login_user(base_url, email, password):
     """Connecte un utilisateur existant"""
     auth = _get_auth_instance(base_url)
     return auth.login_user(email, password)
+
 
 def get_logged_user_info(base_url, token):
     """Récupère les informations de l'utilisateur connecté"""
