aed6b61165d0c223a28d1ef61034d34b7e6fe6e4
Matthieu Falce <matthieu@falce.net>
2025-10-04 13:26:56 +0200

    gitautosync update 429

---
+++ tape_en_cours/demo_api/utils/api.py
@@ -0,0 +1,31 @@
+"""Module API unifié pour demo_api"""
+
+from .api_user import get_users, add_vms_to_users
+from .api_vm import get_vms, create_vm
+from .api_auth import Auth
+
+# Initialisation de l'instance Auth globale
+_auth_instance = None
+
+def _get_auth_instance(base_url):
+    """Retourne l'instance Auth singleton"""
+    global _auth_instance
+    if _auth_instance is None:
+        _auth_instance = Auth(base_url)
+    return _auth_instance
+
+# Wrapper functions pour simplifier l'utilisation
+def create_user(base_url, name, email, password):
+    """Crée un nouvel utilisateur"""
+    auth = _get_auth_instance(base_url)
+    return auth.create_user(name, email, password)
+
+def login_user(base_url, email, password):
+    """Connecte un utilisateur existant"""
+    auth = _get_auth_instance(base_url)
+    return auth.login_user(email, password)
+
+def get_logged_user_info(base_url, token):
+    """Récupère les informations de l'utilisateur connecté"""
+    auth = _get_auth_instance(base_url)
+    return auth.get_logged_user_info(token)
