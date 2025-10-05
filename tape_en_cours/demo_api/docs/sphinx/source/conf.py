1dcabd1208f94c547790307444eecd343f3b59cc
Matthieu Falce <matthieu@falce.net>
2025-10-05 16:53:37 +0200

    gitautosync update 59

--- tape_en_cours/demo_api/docs/sphinx/source/conf.py
+++ tape_en_cours/demo_api/docs/sphinx/source/conf.py
@@ -77,11 +77,6 @@ html_permalinks_icon = "§"
 html_css_files = []
 html_js_files = []
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
-
 # Configuration pour renommer _static en static pour GitHub Pages
 # GitHub Pages peut avoir des problèmes avec les dossiers commençant par _
 html_static_path = ["_static"]
